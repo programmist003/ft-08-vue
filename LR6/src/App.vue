351636b1679d8b3e8d59090adf43c94dec1f004e
programmist <programmist.030@gmail.com>
2025-04-18 02:34:48 +0400

    LR 6 App.js: fixing errors

--- LR6/src/App.vue
+++ LR6/src/App.vue
@@ -1,28 +1,21 @@
-<script>
+<script setup>
 import { ref, onMounted } from 'vue';
 import Emoji from './components/Emoji.vue';
 
-export default {
-  setup() {
-    const emojis = ref([]);
-    const fetchEmojis = async () => {
-      try {
-        const response = await fetch('https://emojihub.yurace.pro/api/all');
-        const data = await response.json();
-        emojis.value = data;
-      } catch (error) {
-        console.error(error);
-      }
-    };
-    onMounted(() => {
-      fetchEmojis();
-    });
-    return { emojis };
-  },
-};
+
+const emojis = ref([]);
+try {
+  let data = await fetch('https://emojihub.yurace.pro/api/all').then((response) => {return response.json()});
+  emojis.value = data;
+  console.log(emojis.value[0]);
+} catch (error) {
+  console.error(error);
+}
+
+
 </script>
 <template>
   <div>
-    <Emoji :emoji="emojis[0]" />
+    <Emoji :emoji="emojis.values[0]" />
   </div>
 </template>
