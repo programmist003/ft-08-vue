8b92c0329690b23a7ce7445e94242bec22b2ae54
programmist <programmist.030@gmail.com>
2025-04-18 02:14:26 +0400

    LR6 start: Emoji.vue

---
+++ LR6/src/App.vue
@@ -0,0 +1,27 @@
+<script>
+import { ref, onMounted } from 'vue';
+
+export default {
+  setup() {
+    const emojis = ref([]);
+    const fetchEmojis = async () => {
+      try {
+        const response = await fetch('https://emojihub.yurace.pro/api/all');
+        const data = await response.json();
+        emojis.value = data;
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    onMounted(() => {
+      fetchEmojis();
+    });
+    return { emojis };
+  },
+};
+</script>
+<template>
+  <div>
+    <Emoji :emoji="emojis[0]" />
+  </div>
+</template>
---
+++ LR6/src/components/Emoji.vue
@@ -0,0 +1,17 @@
+<template>
+    <span>{{ htmlCode[0] }}</span>
+</template>
+  
+<script>
+  export default {
+    props: {
+      emoji: {
+        type: Object,
+        required: true,
+      },
+    },
+    setup(props) {
+      return { htmlCode: props.emoji.htmlCode };
+    },
+  };
+</script>
